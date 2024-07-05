23b99e6569b10a6a86ac397ae8dd222a91c5cb59
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-05 13:12:10 -0700

    setup tile_pile class and some basic cards

---
+++ custom_resources/tile.gd
@@ -0,0 +1,14 @@
+class_name Tile
+extends Resource
+
+enum Type {suit, dragon, wind}
+enum Suit {manzu, pinzu, souzu, none}
+enum Wind {East, North, South, West, none}
+enum Dragon {Green, Red, White, none}
+
+@export_group("Tile Attributes")
+@export var id: String
+@export var type: Type
+@export var value: int
+@export var wind: Wind
+@export var dragon: Dragon
---
+++ custom_resources/tile_pile.gd
@@ -0,0 +1,31 @@
+class_name TilePile
+extends Node
+
+signal tile_pile_size_changed(tile_amount)
+
+@export var tiles: Array[Tile] = []
+
+func empty() -> bool:
+	return tiles.is_empty()
+	
+func draw_tile() -> Tile:
+	var tile = tiles.pop_front()
+	tile_pile_size_changed.emit(tiles.size())
+	return tile
+	
+func add_tile(tile: Tile):
+	tiles.append(tile)
+	tile_pile_size_changed.emit(tiles.size())
+	
+func shuffle() -> void:
+	tiles.shuffle()
+	
+func clear() -> void:
+	tiles.clear()
+	tile_pile_size_changed.emit(tiles.size())
+	
+func _to_string() -> String:
+	var _tile_strings: PackedStringArray = []
+	for i in range(tiles.size()):
+		_tile_strings.append("%s: %s" % [i+1, tiles[i].id])
+	return "\n".join(_tile_strings)
